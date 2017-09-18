3d44e4dc3f08d75715e47778f3d1c512c8893103
Ayush <iius.mdr@gmail.com>
2017-09-18 22:52:11 +1000

    Ayush- unit test file for AdhocTicket
    File for unit testing for AdhocTicket has been created.

---
+++ src/src/bcccp/tickets/adhoc/AdhocTicketUnitTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.bcccp.tickets.adhoc;
+import bcccp.tickets.adhoc.AdhocTicket;
+/**
+ *
+ * @author Ayush
+ */
+public class AdhocTicketUnitTest {
+    public static void main(String[] args){
+        AdhocTicket adTic = new AdhocTicket("jeevan",123,"abcd1234");
+        
+        System.out.println(adTic.getBarcode());
+        System.out.println(adTic.getCarparkId());
+    }
+}
