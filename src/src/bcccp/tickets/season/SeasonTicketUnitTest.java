d671d040c5c05f77762c8c3aedaa727628fc5bf6
Ayush <iius.mdr@gmail.com>
2017-09-19 00:55:49 +1000

    Ayush:code added in unit test for seasonticket
    unit testing code for season ticket has been added and the test is conducted.

--- src/src/bcccp/tickets/season/SeasonTicketUnitTest.java
+++ src/src/bcccp/tickets/season/SeasonTicketUnitTest.java
@@ -4,11 +4,20 @@
  * and open the template in the editor.
  */
 package src.bcccp.tickets.season;
+import bcccp.tickets.season.SeasonTicket;
 
 /**
  *
  * @author Ayush
  */
 public class SeasonTicketUnitTest {
-    
+    public static void main(String[] args){
+    SeasonTicket seasonTic = new SeasonTicket("1234","654", 900, 500);
+        
+        System.out.println(seasonTic.getId());
+        System.out.println(seasonTic.getCarparkId());
+        System.out.println(seasonTic.getStartValidPeriod());
+        System.out.println(seasonTic.getEndValidPeriod());
+
+    }
 }
