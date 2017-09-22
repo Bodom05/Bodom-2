4829b918fc8b268ab0c67fb511680e41eb99c35e
Ayush <iius.mdr@gmail.com>
2017-09-22 10:02:29 +1000

    Ayush: carpark testing
    carpark testing with different values

--- src/src/bcccp/carpark/CarparkUnitTest.java
+++ src/src/bcccp/carpark/CarparkUnitTest.java
@@ -13,7 +13,7 @@ import bcccp.carpark.Carpark;
 public class CarparkUnitTest {
     public static void main(String[] args){
         System.out.println("Creating an instance of Carpark class...");
-        Carpark cp= new Carpark("Sunil",1000,null,null);
+        Carpark cp= new Carpark("Ayush",1000,null,null);
         System.out.println("An instance of Carpark class is created...");
         System.out.println("Testing the getName() method of carpark class");
         System.out.println(cp.getName());
