bde323bf8dc943943ea6617d758ae5281c57b518
Ayush <iius.mdr@gmail.com>
2017-09-22 22:09:12 +1000

    Ayush: EntryControllerIntegrationTesting
    Integration testing of entryController

--- src/src/bcccp/carpark/entry/EntryControllerIntegrationTesting.java
+++ src/src/bcccp/carpark/entry/EntryControllerIntegrationTesting.java
@@ -4,15 +4,21 @@
  * and open the template in the editor.
  */
 package src.bcccp.carpark.entry;
-
+import bcccp.carpark.CarSensor;
+import src.bcccp.carpark.entry.EntryController;
 import bcccp.carpark.Carpark;
+import src.bcccp.carpark.Gate;
 import bcccp.carpark.ICarSensor;
 import bcccp.carpark.ICarSensorResponder;
+import bcccp.carpark.ICarpark;
+import bcccp.carpark.ICarparkObserver;
 import src.bcccp.carpark.IGate;
 import bcccp.tickets.adhoc.AdhocTicketDAO;
 import bcccp.tickets.adhoc.AdhocTicketFactory;
+import bcccp.tickets.adhoc.IAdhocTicket;
 import bcccp.tickets.adhoc.IAdhocTicketDAO;
 import bcccp.tickets.adhoc.IAdhocTicketFactory;
+import bcccp.tickets.season.ISeasonTicketDAO;
 import bcccp.tickets.season.IUsageRecordFactory;
 import bcccp.tickets.season.SeasonTicketDAO;
 import bcccp.tickets.season.UsageRecordFactory;
@@ -22,16 +28,39 @@ import bcccp.tickets.season.UsageRecordFactory;
  * @author Ayush
  */
 public class EntryControllerIntegrationTesting {
-    public static void main(String[] args) {
-    IAdhocTicketFactory adhocTicketFactory = new AdhocTicketFactory();
-    IAdhocTicketDAO adhocTicketDAO = new AdhocTicketDAO(adhocTicketFactory);
-    IUsageRecordFactory factory = new UsageRecordFactory();
-    SeasonTicketDAO seasonTicketDAO = new SeasonTicketDAO(factory);
-    Carpark carpark = new Carpark("c123", 200, adhocTicketDAO, seasonTicketDAO);
-//    IGate igate = new IGate();
-//    ICarSensor carsensor = new ICarSensor();
-    
-    IEntryUI ientryui = new IEntryUI();
-    EntryController entry = new EntryController(carpark, igate, null, null, ientryui);
-    }
+   public static void main(String[] args){
+        System.out.println("Creating an instance of IAdhocticketFactory class...");
+        IAdhocTicketFactory adhocticketfactory = new AdhocTicketFactory();
+        System.out.println("Creating an instance of IAdhocTicketDAO class...");
+        IAdhocTicketDAO iadhocticketdao = new AdhocTicketDAO(adhocticketfactory);
+        System.out.println("Creating an instance of IUsageRecordFactory class...");
+        IUsageRecordFactory usagerecordfactory = new UsageRecordFactory();
+        System.out.println("Creating an instance of ISeasonTicketDAO class...");
+        ISeasonTicketDAO iseasonticketdao = new SeasonTicketDAO(usagerecordfactory);
+        System.out.println("Creating an instnce of Carpark class...");
+        Carpark cp =new Carpark("Bathrust",1000,iadhocticketdao,iseasonticketdao);
+        System.out.println("Creating an instance of igate class...");
+        IGate igate = new Gate(20,20);
+        System.out.println("Creating an instance of insideCarSensor class..");
+        ICarSensor insidecarsensor = new CarSensor("bathrust123",10,10);
+        System.out.println("Creating an instance of outside car sensoor");
+        ICarSensor outsidecarsensor = new CarSensor("bathrust123",15,15);
+        System.out.println("Creating an instanace of entryUi class...");
+        IEntryUI ientryui = new EntryUI(40,40);
+        System.out.println("Creating an instance of EntryController class...");
+        EntryController EC=new EntryController(cp,igate,outsidecarsensor,insidecarsensor,ientryui);
+        System.out.println("Testing the log method..");
+//        EC.log("Ayush Integration testing");
+        System.out.println("Testing the carEventDetected method...");
+        EC.carEventDetected("Ayush", true);
+         EC.buttonPushed();
+        System.out.println("Testing the ticketInserted method..");
+        EC.ticketInserted("A3");
+        System.out.println("Testing the notifyCarpark event..");
+        EC.notifyCarparkEvent();
+        System.out.println("Testing the ticket taken method...");
+        EC.ticketTaken();
+        System.out.println("Tested Successfully");
+       
+   } 
 }
