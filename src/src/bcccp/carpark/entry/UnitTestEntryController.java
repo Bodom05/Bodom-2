71b59589e680aaa785c96970267d1e871f8f7f85
Ayush <iius.mdr@gmail.com>
2017-09-22 10:47:11 +1000

    Ayush: unt testing for entrycontroller
    unit testing for entry controller

--- src/src/bcccp/carpark/entry/UnitTestEntryController.java
+++ src/src/bcccp/carpark/entry/UnitTestEntryController.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package src.bcccp.carpark.entry;
-import bcccp.carpark.entry.EntryController;
+import src.bcccp.carpark.entry.EntryController;
 
 /**
  *
@@ -14,7 +14,7 @@ public class UnitTestEntryController {
     public static void main(String[] args){
         EntryController entry = new EntryController();
         entry.carEventDetected("123", true);
-        System.out.println(entry.getBarcode());
-        System.out.println(entry.getCarparkId());
+//        System.out.println(entry.get());
+//        System.out.println(entry.getCarparkId());
     }
 }
